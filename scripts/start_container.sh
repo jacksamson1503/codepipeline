3f45ae2bdde457084bc323e678ae4fbbae74d5a1
Jack Samson.c <jacksamsonc@gmail.com>
2026-09-03 13:10:13 +0530

    Add simple container start script

---
+++ scripts/start_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+IMAGE_NAME="YOUR_DOCKER_USERNAME/simple-python-flask-app:latest"
+
+docker pull "$IMAGE_NAME"
+docker run -d --name myapp -p 5000:5000 "$IMAGE_NAME"
+
+echo "Container started successfully."
