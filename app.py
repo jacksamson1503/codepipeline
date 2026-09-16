3abd31a4c28a597e5d639ec620991e7eb7fe9d69
Jack Samson.c <jacksamsonc@gmail.com>
2026-09-16 13:49:07 +0530

    Simplify Flask app for ECS deployment

--- app.py
+++ app.py
@@ -3,10 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # 0.0.0.0 so the container's exposed port is reachable from the EC2 host / internet,
-    # not just from inside the container.
     app.run(host='0.0.0.0', port=5000)
