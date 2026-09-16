941d9e880d9098dcd1aee37a9771c8b51715a95d
Jack Samson.c <jacksamsonc@gmail.com>
2026-09-16 13:49:38 +0530

    Remove obsolete CodeDeploy start script

--- scripts/start_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-IMAGE_NAME="jack1503/simple-python-flask-app:latest"
-
-docker pull "$IMAGE_NAME"
-docker run -d --name myapp -p 5000:5000 "$IMAGE_NAME"
-
-echo "Container started successfully."
